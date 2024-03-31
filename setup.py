87a6a90820251291fee2248cc9c5b7099faafd19
Touhid Mia <touhid@banglafighter.com>
2024-03-31 16:15:30 +0600

    Updated and added new CORS Configuration

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ env = os.environ.get('source')
 
 
 def get_dependencies():
-    dependency = ["python-barcode==0.15.1", "Flask-SocketIO==5.3.6"]
+    dependency = ["python-barcode==0.15.1", "Flask-SocketIO==5.3.6", "weasyprint==61.2"]
 
     if env and env == "dev":
         return dependency
